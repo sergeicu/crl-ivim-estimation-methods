0103c15ea1a28969f178a7147aa41b1403df9fb9
sergeicu <serge.vasylechko@gmail.com>
2022-03-14 15:34:13 -0400

    Update download_and_convert_dicoms.sh

--- preprocessing/download_and_convert_dicoms.sh
+++ preprocessing/download_and_convert_dicoms.sh
@@ -3,11 +3,12 @@
 # Step 1 - download DICOMs from BCH scanners using Patient MRN and DATE of Acquisition 
 MRN=<patient_MRN>
 DATE=<YYYYMMDD>
-savedir=<path_to_save_files_to>
+savedir=<path_to_save_DICOM_files_to>
 bash retrieve2.sh $MRN $DATE $savedir
 
 # Step 2 - convert DICOM files into NIFTI - perform this on EACH individual DICOM directory that contains DWI files (i.e. scandir)
-scandir=<path_to_scandir_with_dicoms_for_DWI_scan>
+scandir=<path_to_scan_directory_that_contains_dicoms_of_the_DWI_scan>
+name=<custom_save_name_for_output_files_eg_subject1>
 cd $savedir
 niidir=$scandir/nii
 mkdir $scandir/nii
